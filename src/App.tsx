b17f9ae7b8ca4fc1e1db8098d2e5c50d6ec7c6d6
Maksym Morenko <morenko.maxim@gmail.com>
2022-10-27 21:03:35 +0300

    компонент статистики

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Container } from 'components/Container'
 import { TheHeader } from 'components/TheHeader';
 import { Search } from 'components/Search';
+import { UserCard } from 'components/UserCard';
+import { defaultUser } from 'mock';
 
 function App() {
   return (
     <Container>
       <TheHeader/>
       <Search hasError onSubmit={() => {}}/>
+      <UserCard 
+        {...defaultUser}
+      />
     </Container>
   );
 }
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "michey85",
+  "id": 36237995,
+  "avatar": "https://avatars.githubusercontent.com/u/36237995?v=4",
+  "name": "Mikhail Nepomnyashchiy",
+  "company": "EPAM",
+  "blog": "mishanep.com",
+  "location": "Malaga",
+  "bio": "React/Vue developer",
+  "twitter": "pcgramota",
+  "repos": 60,
+  "followers": 184,
+  "following": 5,
+  "created": "2018-02-07T16:18:45Z",
+}
